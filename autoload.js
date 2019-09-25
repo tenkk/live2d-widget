b640820adbb0774a7c55d9d8ffc9415e8d6a4a41
tenkk <55651353+tenkk@users.noreply.github.com>
2019-09-25 09:30:59 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,9 +1,7 @@
-//注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://cdn.statically.io/gh/tenkk/live2d-widget/master/";
-//const live2d_path = "/live2d-widget/";
 
-//加载waifu.css
-$("<link>").attr({ href: live2d_path + "waifu.css", rel: "stylesheet" }).appendTo("head");
+const live2d_path = "https://cdn.statically.io/gh/tenkk/live2d-widget/master/";
+
+$("<link>").attr({ href: live2d_path + "waifu.css?env=dev", rel: "stylesheet" }).appendTo("head");
 
 //加载live2d.min.js
 $.ajax({
@@ -14,16 +12,11 @@ $.ajax({
 
 //加载waifu-tips.js
 $.ajax({
-	url: live2d_path + "waifu-tips.js",
+	url: live2d_path + "waifu-tips.js?env=dev",
 	dataType: "script",
 	cache: true
 });
 
 //初始化看板娘，会自动加载指定目录下的waifu-tips.json
 $(window).on("load", function() {
-	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
-});
-//initWidget第一个参数为waifu-tips.json的路径
-//第二个参数为api地址（无需修改）
-//api后端可自行搭建，参考https://github.com/fghrsh/live2d_api
-
+	initWidget(live2d_path + "waifu-tips.json?env=dev", "https://

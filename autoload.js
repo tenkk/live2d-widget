d413f0cece239c9e43590df563bf5605653a4d8c
tenkk <55651353+tenkk@users.noreply.github.com>
2019-09-25 22:10:33 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,23 +1,23 @@
 
 const live2d_path = "https://cdn.statically.io/gh/tenkk/live2d-widget/master/";
 
-$("<link>").attr({ href: live2d_path + "waifu.css", rel: "stylesheet" }).appendTo("head");
+$("<link>").attr({ href: live2d_path + "waifu.css?env=dev", rel: "stylesheet" }).appendTo("head");
 
 //加载live2d.min.js
 $.ajax({
-	url: live2d_path + "live2d.min.js",
+	url: live2d_path + "live2d.min.js?env=dev",
 	dataType: "script",
 	cache: true
 });
 
 //加载waifu-tips.js
 $.ajax({
-	url: live2d_path + "waifu-tips.js",
+	url: live2d_path + "waifu-tips.js?env=dev",
 	dataType: "script",
 	cache: true
 });
 
 //初始化看板娘，会自动加载指定目录下的waifu-tips.json
 $(window).on("load", function() {
-	initWidget(live2d_path + "waifu-tips.json", "https://live2d.fghrsh.net/api");
+	initWidget(live2d_path + "waifu-tips.json?env=dev", "https://live2d.fghrsh.net/api");
 });
